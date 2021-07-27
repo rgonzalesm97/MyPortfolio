656a4a704a96552181a4fe9693005b8ee5e6a9e0
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-07-27 17:27:33 -0500

    ease pictures and animatios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,19 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('start', [
+
+      transition(':enter', [
+        style({transform: 'translateY(20%)', opacity: 0}),
+        animate('1000ms ease-out')
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
